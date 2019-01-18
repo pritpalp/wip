f4a57fbf1880a1c33e99b6a23e1060ff38571455
pritpalp <36885087+pritpalp@users.noreply.github.com>
2019-01-18 16:55:32 +0000

    Updated get_fd_per_pid.py

--- utils/fd/get_fd_per_pid.py
+++ utils/fd/get_fd_per_pid.py
@@ -1,13 +1,15 @@
 import os
 import resource
 
+# needs to run as sudo/root to check all process
 print "Open file limits (soft, hard):", resource.getrlimit(resource.RLIMIT_NOFILE)
+print "Open file limit from /proc/sys/fs/file-max: ", open("/proc/sys/fs/file-max").read().strip()
+print "Open file descriptors from /proc/sys/fs/file-nr: ", open("/proc/sys/fs/file-nr").read().strip()
 
 pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
 
 for pid in pids:
   try:
-    #print pid
     pid = pid.strip()
     process = os.popen("readlink /proc/" + pid + "/fd/*")
     cmd = open("/proc/" + pid + "/cmdline").read()
@@ -15,7 +17,7 @@ for pid in pids:
     i = 0
     for fd in fds:
       if not (fd.startswith("socket") or fd.startswith("pipe") or fd.startswith("/dev/")):
-        i = i + 1
+        i += 1  # we don't want to count everything
     if i > 0:
       print (i, pid, cmd)
   except IOError:  # proc has already terminated

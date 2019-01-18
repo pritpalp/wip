4c9c0823fb85285e9b0a6cf5006a3c8170926c02
pritpalp <36885087+pritpalp@users.noreply.github.com>
2019-01-18 16:12:40 +0000

    updated get_fd_per_pid.py

--- utils/fd/get_fd_per_pid.py
+++ utils/fd/get_fd_per_pid.py
@@ -7,9 +7,16 @@ pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
 
 for pid in pids:
   try:
-    print pid
+    #print pid
     pid = pid.strip()
     process = os.popen("readlink /proc/" + pid + "/fd/*")
-    print (process.read())
+    cmd = open("/proc/" + pid + "/cmdline").read()
+    fds = process.read().strip().splitlines()
+    i = 0
+    for fd in fds:
+      if not (fd.startswith("socket") or fd.startswith("pipe") or fd.startswith("/dev/")):
+        i = i + 1
+    if i > 0:
+      print (i, pid, cmd)
   except IOError:  # proc has already terminated
     continue

af2aa918bef9ad280ae4643d55f7196bfa73e34b
pritpalp <36885087+pritpalp@users.noreply.github.com>
2019-01-21 12:06:32 +0000

    Print a message when more than 80% of file descriptors are used by a process

--- utils/fd/get_fd_per_pid.py
+++ utils/fd/get_fd_per_pid.py
@@ -3,8 +3,11 @@ import resource
 
 # needs to run as sudo/root to check all process
 print "Open file limits (soft, hard):", resource.getrlimit(resource.RLIMIT_NOFILE)
-print "Open file limit from /proc/sys/fs/file-max: ", open("/proc/sys/fs/file-max").read().strip()
+# print "Open file limit from /proc/sys/fs/file-max: ", open("/proc/sys/fs/file-max").read().strip()
 print "Open file descriptors from /proc/sys/fs/file-nr: ", open("/proc/sys/fs/file-nr").read().strip()
+
+# get the soft limit and store it
+soft_limit = resource.getrlimit(resource.RLIMIT_NOFILE)[0]
 
 pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
 
@@ -19,6 +22,9 @@ for pid in pids:
       if not (fd.startswith("socket") or fd.startswith("pipe") or fd.startswith("/dev/")):
         i += 1  # we don't want to count everything
     if i > 0:
-      print (i, pid, cmd)
+      percentage_used = round(float(i) / soft_limit * 100, 2)
+      # only print out values if nearing the limit
+      if percentage_used > 80.00:
+        print "PID ", pid, " (", cmd, ") has used", percentage_used, "% of the available file descritptors (", i, "/", soft_limit, ")"
   except IOError:  # proc has already terminated
     continue

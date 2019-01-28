f1c35a2a7aa3b725ab2f2d6b1b04e87f26826979
pritpalp <36885087+pritpalp@users.noreply.github.com>
2019-01-28 12:32:13 +0000

    Updated to get over some permission denied messages

--- utils/fd/get_fd_per_pid.py
+++ utils/fd/get_fd_per_pid.py
@@ -10,8 +10,6 @@ parser.add_argument("-p", dest="percent_limit", default=80, type=int)
 args = parser.parse_args()
 percent_limit = args.percent_limit
 
-# print "Open file limits (soft, hard):", resource.getrlimit(resource.RLIMIT_NOFILE)
-# print "Open file limit from /proc/sys/fs/file-max: ", open("/proc/sys/fs/file-max").read().strip()
 # print "Open file descriptors from /proc/sys/fs/file-nr: ", open("/proc/sys/fs/file-nr").read().strip()
 
 # get the soft limit and store it
@@ -27,19 +25,25 @@ pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
 for pid in pids:
   try:
     pid = pid.strip()
-    process = os.popen("readlink /proc/" + pid + "/fd/*")
-    # cmd = open("/proc/" + pid + "/cmdline").read()  # get the cmd that's running
-    cmd = os.readlink("/proc/" + pid + "/exe")
-    path, file = os.path.split(cmd)
-    fds = process.read().strip().splitlines()  # list the contents of /proc/[pid]/fd/
+    cmd = open("/proc/" + pid + "/cmdline").read()  # get the cmd that's running
+    cmd = cmd.split()[0]
+    # cmd = os.readlink("/proc/" + pid + "/exe")  # this doesn't work very well, can get permission denied
+    # path, cmd = os.path.split(cmd)
+    # process = os.popen("readlink /proc/" + pid + "/fd/*")
+    # fds = process.read().strip().splitlines()  # list the contents of /proc/[pid]/fd/
+    # above code can also get permission denied, so eventually used os.walk() to get the names
+    fds = []
+    for (dirpath, dirname, filenames) in os.walk("/proc/" + pid + "/fd/"):
+      fds.extend(filenames)
+      break
     i = 0
     for fd in fds:
-      if not (fd.startswith("socket") or fd.startswith("pipe") or fd.startswith("/dev/")):
+      if not (fd.startswith("socket") or fd.startswith("pipe")):
         i += 1  # we don't want to count everything
     if i > 0:
       percentage_used = round(float(i) / soft_limit * 100, 2)
       # only print out values if nearing the limit
       if percentage_used > percent_limit:
-        print "PID ", pid, " (", file, ") has used", percentage_used, "% of the available file descriptors (", i, "/", soft_limit, ")"
+        print "PID ", pid, " (", cmd, ") has used", percentage_used, "% of the available file descriptors (", i, "/", soft_limit, ")"
   except IOError:  # proc has already terminated
     continue

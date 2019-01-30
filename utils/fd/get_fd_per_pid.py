ac8c5befd38d5de0378e0b98d6ad50d1e523db21
pritpalp <36885087+pritpalp@users.noreply.github.com>
2019-01-30 11:26:48 +0000

    Added help messages for the parameters Removed the command from the output, so we just display the pid and numbers Removed some unused commented code Changed code to read the soft limits for the process for the comparison (defaulting to the system values)

--- utils/fd/get_fd_per_pid.py
+++ utils/fd/get_fd_per_pid.py
@@ -5,12 +5,18 @@ import argparse
 # needs to run as sudo/root to check all process
 
 # Pull an argument to get the percentage limit you want to compare to, defaults to 80
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", dest="percent_limit", default=80, type=int)
+parser = argparse.ArgumentParser(description='Get the open file descriptors per process '
+                                             'and print a messaage when its above a specified threshold')
+parser.add_argument("-p", dest="percent_limit", default=80,
+                    help='Percentage to compare the file descriptor usage to, defaults to 80', type=int)
 args = parser.parse_args()
+# print out usage if incorrect arguments are passed
+if not vars(args):
+  parser.print_help()
+  parser.exit(1)
 percent_limit = args.percent_limit
 
-# print "Open file descriptors from /proc/sys/fs/file-nr: ", open("/proc/sys/fs/file-nr").read().strip()
+# Pull the max open fd's and currently open fd's
 file_nr = open("/proc/sys/fs/file-nr").read().strip()
 open_fd = file_nr.split()[0]
 max_fd = file_nr.split()[-1]
@@ -18,7 +24,7 @@ percentage_max_fd = round(float(open_fd) / float(max_fd) * 100, 2)
 if percentage_max_fd > percent_limit:
   print "Total open file descriptors have reached", percentage_max_fd, "% of the system max (", open_fd, " of ", max_fd, ")"
 
-# get the soft limit and store it
+# get the soft limit and store it, to use as backup
 soft_limit = resource.getrlimit(resource.RLIMIT_NOFILE)[0]
 
 pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
@@ -26,19 +32,12 @@ pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
 for pid in pids:
   try:
     pid = pid.strip()
-    cmd = open("/proc/" + pid + "/cmdline").read()  # get the cmd that's running
-    # just try to grab the first part of the cmd
-    if cmd.strip():
-      cmd = cmd.split()[0]
-      if ":" in cmd:
-        cmd = cmd.split(":")[0]
-      else:
-        cmd = cmd.split("/")[-1]
-    # cmd = os.readlink("/proc/" + pid + "/exe")  # this doesn't work very well, can get permission denied
-    # path, cmd = os.path.split(cmd)
-    # process = os.popen("readlink /proc/" + pid + "/fd/*")
-    # fds = process.read().strip().splitlines()  # list the contents of /proc/[pid]/fd/
-    # above code can also get permission denied, so eventually used os.walk() to get the names
+    limits = open("/proc/" + pid + "/limits").readlines()  # get the limits for the proc
+    for line in limits:
+      if line.startswith("Max open files"):
+        soft_limit = int(line.split()[3])  # splitting the line on spaces we want the 4th value
+        break
+    # used os.walk() to get the names in the /proc/[pid]/fd dir
     fds = []
     for (dirpath, dirname, filenames) in os.walk("/proc/" + pid + "/fd/"):
       fds.extend(filenames)
@@ -46,11 +45,11 @@ for pid in pids:
     i = 0
     for fd in fds:
       if not (fd.startswith("socket") or fd.startswith("pipe")):
-        i += 1  # we don't want to count everything
+        i += 1  # we don't want to count everything, can ignore sockets and pipes
     if i > 0:
       percentage_used = round(float(i) / soft_limit * 100, 2)
       # only print out values if nearing the limit
       if percentage_used > percent_limit:
-        print "PID ", pid, " (", cmd, ") has used", percentage_used, "% of the available file descriptors (", i, "/", soft_limit, ")"
+        print "PID ", pid, " has used", percentage_used, "% of the available file descriptors (", i, "/", soft_limit, ")"
   except IOError:  # proc has already terminated
     continue

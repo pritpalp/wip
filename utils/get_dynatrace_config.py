61fa9bb685ba8325d6995eaec5a4c2079c2a99d0
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2019-03-26 11:01:29 +0000

    Update get_dynatrace_config.py
    Add in a little more functionality

--- utils/get_dynatrace_config.py
+++ utils/get_dynatrace_config.py
@@ -1,28 +1,48 @@
 #!/bin/python
 import requests
-import json
 
+# Constants
 API_TOKEN = "?Api-Token="
 API_KEY = ""
 DYNATRACE_URL = ""
 API_VERSION = "/api/config/v1"
 ENVIRONMENT_ID = ""
 
+def write_file(filename, contents):
+  # create a file if we need one
+  f = open(filename, "w+")
+  f.write(contents)
+  f.close
+
+
 def do_request(action):
   req_url = DYNATRACE_URL+ENVIRONMENT_ID+API_VERSION+action+API_TOKEN+API_KEY
   response = requests.get(req_url)
   server_response = ""
   if response.status_code == 200:
-    server_response = json.loads(response.content.decode('utf-8'))
+    server_response = response.content.decode('utf-8')
+  # print out to screen...
   print req_url, "\n"
-  print server_response, "\n\n"
   return server_response
+
 
 # Web application config
 # we only have one application, so can just grab the default config
 action = "/applications/web/default"
 web_app_config = do_request(action)
+write_file("web_app_config.json", web_app_config)
 
-# Anomoly Detection - Applications
+# Anomaly Detection - Applications
 action = "/anomalyDetection/applications"
-app_anomoly_detection = do_request(action)
+app_anomaly_detection = do_request(action)
+write_file("app_anomaly_detection.json", app_anomaly_detection)
+
+# Anomaly Detection - AWS
+action = "/anomalyDetection/aws"
+aws_anomaly_detection = do_request(action)
+write_file("aws_anomaly_detection.json", aws_anomaly_detection)
+
+# Anomaly Detection - DB services
+action = "/anomalyDetection/databaseServices"
+db_anomaly_detection = do_request(action)
+write_file("db_anomaly_detection.json", db_anomaly_detection)

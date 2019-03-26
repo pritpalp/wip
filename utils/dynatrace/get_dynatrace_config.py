5f89e63f4666662d649fda0550ada1ad7e710ddd
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2019-03-26 15:44:41 +0000

    Update get_dynatrace_config.py
    Added some more things...more to come

--- utils/dynatrace/get_dynatrace_config.py
+++ utils/dynatrace/get_dynatrace_config.py
@@ -16,33 +16,40 @@ def write_file(filename, contents):
 
 
 def do_request(action):
-  req_url = DYNATRACE_URL+ENVIRONMENT_ID+API_VERSION+action+API_TOKEN+API_KEY
-  response = requests.get(req_url)
+  response = requests.get(action)
   server_response = ""
   if response.status_code == 200:
     server_response = response.content.decode('utf-8')
-  # print out to screen...
-  print req_url, "\n"
   return server_response
+
+
+def config_to_get(url_part, get_what, params="NONE"):
+  if params is "NONE":
+    action = DYNATRACE_URL+ENVIRONMENT_ID+API_VERSION+url_part+API_TOKEN+API_KEY
+  else:
+    action = DYNATRACE_URL+ENVIRONMENT_ID+API_VERSION+url_part+API_TOKEN+API_KEY+params
+  filename = get_what + ".json"
+  get_what = do_request(action)
+  write_file(filename, get_what)
+  print action + "\n"
 
 
 # Web application config
 # we only have one application, so can just grab the default config
-action = "/applications/web/default"
-web_app_config = do_request(action)
-write_file("web_app_config.json", web_app_config)
+config_to_get("/applications/web/default", "web_app_config")
 
 # Anomaly Detection - Applications
-action = "/anomalyDetection/applications"
-app_anomaly_detection = do_request(action)
-write_file("app_anomaly_detection.json", app_anomaly_detection)
+config_to_get("/anomalyDetection/applications", "app_anomaly_detection")
 
 # Anomaly Detection - AWS
-action = "/anomalyDetection/aws"
-aws_anomaly_detection = do_request(action)
-write_file("aws_anomaly_detection.json", aws_anomaly_detection)
+config_to_get("/anomalyDetection/aws", "aws_anomaly_detection")
 
 # Anomaly Detection - DB services
-action = "/anomalyDetection/databaseServices"
-db_anomaly_detection = do_request(action)
-write_file("db_anomaly_detection.json", db_anomaly_detection)
+config_to_get("/anomalyDetection/databaseServices", "db_anomaly_detection")
+
+# Anomaly Detection - Hosts
+config_to_get("/anomalyDetection/hosts", "host_anomaly_detection")
+
+# Anomaly Detection - Metric events
+# **** Need to expand this out and get the individual entities ****
+config_to_get("/anomalyDetection/metricEvents", "metric_anomaly_detection", "&includeEntityFilterMetricEvents=true")

00cf0c6e78a92eddd9255a1a153269d2471df798
Pritpal Panesar <36885087+pritpalp@users.noreply.github.com>
2019-04-01 15:44:52 +0100

    Update get_dynatrace_config.py
    Updated to read config from a file for downloading config

--- utils/dynatrace/get_dynatrace_config.py
+++ utils/dynatrace/get_dynatrace_config.py
@@ -2,10 +2,14 @@ import requests
 import os
 import json
 import io
+import yaml
 
 # Constants
 API_STRING = "?Api-Token="
 API_VERSION = "/api/config/v1"
+
+# Pulled from environement variables
+CONFIG_FILE = os.getenv("DYNATRACE_CONFIG_FILE")
 API_KEY = os.getenv("API_KEY")
 DYNATRACE_URL = os.getenv("DYNATRACE_URL")
 ENVIRONMENT_ID = os.getenv("ENVIRONMENT_ID")
@@ -24,18 +28,17 @@ def do_request(action):
   return requests.get(action)
 
 
-def config_to_get(url_part, get_what, params="NONE", multipart="FALSE"):
+def dynatrace_config_to_get(url_part, get_what, params="NOPARAMS", multipart="ISNT_MULTIPART"):
   # construct the url, send the request to the server, and write out the response to file
-  if params is "NONE":
+  if "NOPARAMS" in params:
     action = DYNATRACE_URL+ENVIRONMENT_ID+API_VERSION+url_part+API_STRING+API_KEY
   else:
     action = DYNATRACE_URL+ENVIRONMENT_ID+API_VERSION+url_part+API_STRING+API_KEY+params
   filename = get_what + ".json"
   server_response = do_request(action)
   print action + "\n"
-  print server_response.content.decode('utf-8')
   write_file(filename, server_response.content.decode('utf-8'))
-  if multipart is "TRUE":
+  if "IS_MULTIPART" in multipart:
     json_repsonse = json.loads(server_response.content.decode('utf-8'))
     json_root_element = "values"
     # hack for dashboards json, has a dashboards 'object' rather than 'values'
@@ -48,42 +51,28 @@ def config_to_get(url_part, get_what, params="NONE", multipart="FALSE"):
       # hack for dashboards json, name can be the same for multiple users/owners
       if "dashboards" in url_part:
         new_get_what = (get_what + "__" + ids['name'] + "___" + ids['owner']).replace(" ", "_")
-      config_to_get(new_url_part, new_get_what)
+      dynatrace_config_to_get(new_url_part, new_get_what)
 
 
-# Web application config
-# we only have one application, so can just grab the default config
-config_to_get("/applications/web/default", "web_app_config")
+def get_config(config_file_name):
+  # read in the config file and pull it into files ready to commit to a repo
+  action_name = action_params = action_url = action_multipart = ""
+  with open(config_file_name, 'r') as f:
+    try:
+      docs = yaml.load_all(f, Loader=yaml.FullLoader)
+      for doc in docs:
+        for k,v in doc.items():
+          if "name" in k:
+            action_name = v
+          if "url" in k:
+            action_url = v
+          if "params" in k:
+            action_params = v
+          if "multipart" in k:
+            action_multipart = v
+        dynatrace_config_to_get(action_url, action_name, action_params, action_multipart)
+    except yaml.YAMLError as exc:
+      print(exc)
 
-# Anomaly Detection - Applications
-config_to_get("/anomalyDetection/applications", "app_anomaly_detection")
 
-# Anomaly Detection - AWS
-config_to_get("/anomalyDetection/aws", "aws_anomaly_detection")
-
-# Anomaly Detection - DB services
-config_to_get("/anomalyDetection/databaseServices", "db_anomaly_detection")
-
-# Anomaly Detection - Hosts
-config_to_get("/anomalyDetection/hosts", "host_anomaly_detection")
-
-# Anomaly Detection - Services
-config_to_get("/anomalyDetection/services", "services_anomaly_detection")
-
-# Anomaly Detection - Metric events
-config_to_get("/anomalyDetection/metricEvents", "metric_anomaly_detection", "&includeEntityFilterMetricEvents=true", "TRUE")
-
-# Application detection configuration
-config_to_get("/applicationDetectionRules", "app_detection_config", "NONE", "TRUE")
-
-# Automatically applied tags
-config_to_get("/autoTags", "auto_tags", "NONE", "TRUE")
-
-# Maintenance windows
-config_to_get("/maintenanceWindows", "maintenance", "NONE", "TRUE")
-
-# Management zones
-config_to_get("/managementZones", "management_zones", "NONE", "TRUE")
-
-# Dashboards
-config_to_get("/dashboards", "dashboards", "NONE", "TRUE")
+get_config(CONFIG_FILE)
